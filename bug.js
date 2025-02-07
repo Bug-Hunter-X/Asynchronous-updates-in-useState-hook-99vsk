5279d2456c55216ad27353529321be5acc36ba89
BugHunter-X <askzenai@gmail.com>
2025-02-07 12:20:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,22 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once on mount
+    console.log('Mounted!');
+  }, []);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
+    console.log(count); // This will log the previous count
+  };
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+```
